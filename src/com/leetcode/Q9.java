510161a9701d5bb4fd4c4733ee1d0899f14d4ab5
Zep <zeptolx@gmail.com>
2021-02-22 19:10:20 +0800

    initial commit

--- src/com/leetcode/Q9.java
+++ src/com/leetcode/Q9.java
@@ -4,8 +4,9 @@ public class Q9 {
     public static void main(String[] args) {
         System.out.println(isPalindrome(12321));
     }
+
     public static boolean isPalindrome(int x) {
-        if (x < 0) return false;
+        if (x < 0 || (x % 10 == 0 && x != 0)) return false;
         String str = String.valueOf(x);
         return new StringBuilder(str).reverse().toString().equals(str);
     }
