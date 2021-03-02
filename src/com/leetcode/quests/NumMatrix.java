2887dfaad4f2e06faa747bfb508b8f67fb79a901
Zep <zeptolx@gmail.com>
2021-03-02 16:06:14 +0800

    initial commit

---
+++ src/com/leetcode/quests/NumMatrix.java
@@ -0,0 +1,25 @@
+package com.leetcode.quests;
+
+public class NumMatrix {
+    public static void main(String[] args) {
+        var obj = new NumMatrix(new int[][]{{3, 0, 1, 4, 2}, {5, 6, 3, 2, 1}, {1, 2, 0, 1, 5}, {4, 1, 0, 1, 7}, {1, 0, 3, 0, 5}});
+        System.out.println(obj.sumRegion(2, 1, 4, 3));
+        System.out.println(obj.sumRegion(1, 2, 2, 4));
+    }
+
+    private final int[][] matrix;
+
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                sum += matrix[i][j];
+            }
+        }
+        return sum;
+    }
+}
